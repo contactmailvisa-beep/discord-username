f678ab787564b1fdd43ed5289f5b4d050e0ad2d0
contactmailvisa-beep <contact.mail.visa@gmail.com>
2025-11-18 10:48:36 +0300

    Update switch.tsx

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -22,7 +22,7 @@ const Switch = React.forwardRef<
       className={cn(
         "pointer-events-none block h-4 w-4 rounded-full bg-[#0f1622] shadow-lg ring-0 transition-transform",
         // تحريك الدائرة
-        "data-[state=unchecked]:translate-x-[2px]",
+        "data-[state=unchecked]:-translate-x-[10px]",
         "data-[state=checked]:translate-x-[22px]"
       )}
     />
