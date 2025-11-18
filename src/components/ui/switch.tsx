33a323f38800761d613691767315efdf05a84309
contactmailvisa-beep <contact.mail.visa@gmail.com>
2025-11-18 10:46:19 +0300

    Update switch.tsx

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as SwitchPrimitives from "@radix-ui/react-switch";
-
 import { cn } from "@/lib/utils";
 
 const Switch = React.forwardRef<
@@ -8,20 +7,28 @@ const Switch = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
-    className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
-      className,
-    )}
-    {...props}
     ref={ref}
+    {...props}
+    className={cn(
+      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors",
+      // لون الخلفية حسب الحالة
+      "data-[state=checked]:bg-blue-500 data-[state=unchecked]:bg-[#1f2a3a]",
+      // باقي التنسيقات
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[22px] data-[state=unchecked]:translate-x-[2px]",
+        "pointer-events-none block h-4 w-4 rounded-full bg-[#0f1622] shadow-lg ring-0 transition-transform",
+        // تحريك الدائرة
+        "data-[state=unchecked]:translate-x-[2px]",
+        "data-[state=checked]:translate-x-[22px]"
       )}
     />
   </SwitchPrimitives.Root>
 ));
+
 Switch.displayName = SwitchPrimitives.Root.displayName;
 
 export { Switch };
